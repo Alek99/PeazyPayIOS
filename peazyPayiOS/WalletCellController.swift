4fd4c34d803e2cb7f6257558353e1f04189d6cab
Alek99 <38776361+Alek99@users.noreply.github.com>
2019-05-05 19:33:14 -0700

    Add files via upload

---
+++ peazyPayiOS/WalletCellController.swift
@@ -0,0 +1,26 @@
+//
+//  WalletCellController.swift
+//  peazyPayiOS
+//
+//  Created by Arman Vaziri on 5/5/19.
+//  Copyright © 2019 Elden Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WalletCellController: UITableViewCell {
+    
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
